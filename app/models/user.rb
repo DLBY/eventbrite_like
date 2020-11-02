e8becc9ec648b839a53622334a4e339853f3b110
DLBY <david.lobry96@gmail.com>
2020-11-02 14:29:21 +0100

    push for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
   has_many :events
   has_many :events, through: :attendances
   has_many :attendances
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
 end
