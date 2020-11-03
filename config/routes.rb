6121bacd45ff184e98052703425b29e072aa5b20
DLBY <david.lobry96@gmail.com>
2020-11-03 15:35:58 +0100

    adding devise + heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  
   root 'events#index'
+
+  resources :users, only: [:show]
 
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
