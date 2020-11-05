fb34468fd0684eaa70a8053d8e33bbc77f2216cb
DLBY <david.lobry96@gmail.com>
2020-11-05 16:45:22 +0100

    heroku push

--- app/controllers/event_pictures_controller.rb
+++ app/controllers/event_pictures_controller.rb
@@ -1,14 +1,9 @@
 class EventPicturesController < ApplicationController
   def create
-  @event = Event.find(params(params_event)
+  @event = Event.find(params[:event_id])
   @event.event_picture.attach(params[:event_picture])
   redirect_to(event_path(@event))
   end
 
-  private
-
-  def params_picture
-    params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
-  end
-
+  
 end
